cd3f88f123b043894ffd055bc883b3f08ec1a3a3
Harshita <h2010singh@gmail.com>
2019-08-10 09:58:50 +0530

    DB connectivity
    Changed settings.py

--- HireSight/HireSight/settings.py
+++ HireSight/HireSight/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'HireSight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "recruitment_system",
+        "HOST" : "localhost",
+        "USER" : "root",
+        "PASSWORD" : "",
     }
 }
 
