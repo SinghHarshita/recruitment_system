51f0c4fd4f32282f863d19e6671584c44894509b
Vignesh Pillai <2017.vignesh.pillai@ves.ac.in>
2019-08-28 03:19:44 +0530

    OAuth with login to dashboard

--- HireSight/HireSight/views.py
+++ HireSight/HireSight/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.sessions.models import Session
+from django.contrib.auth.models import User
+
+from django.db import connection
+from django.urls import reverse
+from django.contrib import messages
+from django.shortcuts import redirect
+
 # Create your views here .........
 def index(request):
     return HttpResponse("HireSight Welcomes You!")
+
+def details(request):
+    user = str(request.user.email)
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * from user where email_id = %s",[user])
+        row1 = list(cursor.fetchall())
+        cursor.execute("SELECT * from company where email_id = %s",[user])
+        row2 = list(cursor.fetchall())
+    if(len(row1) == 1 or len(row2) == 1):
+        return redirect(auth_user)
+
+    return HttpResponse("<h2>Sumedh please make the Form<br>I'm Bored<br>Collect the details like whether he is registering as company or User<br>After that insert the details into the User/Company table</h2>")
+
+def auth_user(request):
+    user = str(request.user.email)
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * from user where email_id = %s",[user])
+        row1 = list(cursor.fetchall())
+        cursor.execute("SELECT * from company where email_id = %s",[user])
+        row2 = list(cursor.fetchall())
+
+    if(len(row1) == 0 and len(row2) == 0):
+        # messages.warning(request,"You are Not Registered....")
+        return redirect('/',kwargs={msg:"You are Not Registered...."})
+    
+    if(len(row1)):
+        return redirect('/applicant/',kwargs={})
+    else:
+        return redirect('comapany/',kwargs={})
+
+    # return HttpResponse(str(len(row)))
