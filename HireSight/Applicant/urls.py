595c842912a855017015379cf5bd1fcf9c758764
Harshita <h2010singh@gmail.com>
2019-09-25 00:41:28 +0530

    Different Funuctionalities
    Added Pages for different functionalities

--- HireSight/Applicant/urls.py
+++ HireSight/Applicant/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name = "applicant_dashboard"),
     url(r'^notifications/$', views.notifications, name = "notifications"),
-    url(r'^test/$', views.test, name="test"),
+    #url(r'^test/$', views.test, name="test"),
+    url(r'^profile/$', views.applicant_profile, name="applicant_profile"),
+    url(r'^history/$', views.applicant_history, name="applicant_history"),
+    url(r'^browse/$', views.browse_jobs_render, name="browse_jobs_render"),
     #url(r'^load_applicant', views.index, name="index")
 ]
--- HireSight/Applicant/views.py
+++ HireSight/Applicant/views.py
@@ -8,7 +8,7 @@ def index(request):
     global data
     #print(request.session["id"])
     with connection.cursor() as cursor :
-        cursor.execute("SELECT * from user where u_id = %s",[request.session["id"]])
+        cursor.execute("SELECT * from user where u_id = {}".format(request.session["id"]))
         data = applicant_data(list(cursor.fetchall())[0])
         #data = list(cursor.fetchall())
         #print(data)
@@ -21,7 +21,7 @@ def applicant_data(data) :
     return {
         "name" : data[1],
         "gender" : data[5],
-        "age" : data[6],
+        "dob" : data[6],
         "phone" : data[7],
         "address" : data[8],
         "following_company" : data[11],
@@ -30,8 +30,14 @@ def applicant_data(data) :
         "notifications" : data[12],
     }
 
-def test(request):
-    return render(request, "test.html")
+def applicant_profile(request):
+    return render(request, "applicant_profile.html", data)
+
+def applicant_history(request):
+    return render(request, "applicant_history.html", data)
+
+def browse_jobs_render(request):
+    return render(request, "browse_jobs.html", data)
 
 def notifications(request) :
     global data
