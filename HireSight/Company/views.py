ecd6b0fa55234fc38b86b145cb8db5b353d98faf
Harshita <h2010singh@gmail.com>
2019-09-25 03:01:30 +0530

    Company Dashboard Skeleton

--- HireSight/Company/views.py
+++ HireSight/Company/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db import connection
 
+data = dict()
 # Create your views here.
 def index(request):
+    global data
     with connection.cursor() as cursor :
         cursor.execute("SELECT * from company where c_id = %s",[request.session["id"]])
         data = company_data(list(cursor.fetchall())[0])
@@ -18,6 +20,24 @@ def company_data(data) :
         "contact" : data[5],
         "descp" : data[6],
     }
+
+def company_notifications(request):
+    return render(request, "company_notifications.html", data)
+
+def company_post_jobs(request):
+    return render(request, "company_post_jobs.html", data)
+
+def company_view_applicants(request):
+    return render(request, "company_view_applicants.html", data)
+
+def company_view_jobs(request):
+    return render(request, "company_view_jobs.html", data)
+
+def company_statistics(request):
+    return render(request, "company_statistics.html", data)
+
+def company_profile(request):
+    return render(request, "company_profile.html", data)
 
 def test(request):
     return render(request, "company_test.html", {})
