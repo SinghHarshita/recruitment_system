6fce2c569c2ddeac6a337f6a377171294c4d6f46
Harshita <h2010singh@gmail.com>
2019-10-01 23:38:09 +0530

    Both Dashboards rendered

--- HireSight/Applicant/views.py
+++ HireSight/Applicant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db import connection
 from django.http import HttpResponse
+import json
 
 data = {}
 # Create your views here.
@@ -12,6 +13,60 @@ def index(request):
         data = applicant_data(list(cursor.fetchall())[0])
         #data = list(cursor.fetchall())
         #print(data)
+    
+    
+    history = dict()
+    # Fetching history
+    with connection.cursor() as cursor :
+        cursor.execute("SELECT * FROM application_status WHERE applicant_status LIKE '%{}%'".format(request.session["id"]))
+        result = cursor.fetchall()
+        #print(result)
+        for _ in result:
+            # for i in _:
+            #     print(i)
+            cursor.execute("SELECT * FROM jobs WHERE j_id = {}".format(_[0]))
+            status = eval(_[1])
+            #status = eval(status[request.session["id"]])
+            #print(status[request.session["id"]]["status"])
+            s = ""
+            if int(status[request.session["id"]]["status"]) == 0:
+                s = "Applied"
+            elif int(status[request.session["id"]]["status"]) == 1:
+                s = "Accepted"
+            elif int(status[request.session["id"]]["status"]) == 2:
+                s = "Rejected"
+            elif int(status[request.session["id"]]["status"]) == 3:
+                s = "On Hold"
+            else:
+                pass
+            
+            #print(cursor.fetchall())
+            res = cursor.fetchall()
+
+            for i in res:
+                job_details = eval(i[2])
+                #print(job_details)
+            
+                cursor.execute("SELECT * FROM company WHERE c_id = {}".format(i[1]))
+                #print(cursor.fetchall())
+                company = cursor.fetchall()[0]
+                company_details = {
+                    "c_id" : company[0],
+                    "c_name" : company[1],
+                    "c_address" : company[2],
+                    "c_email" : company[3],
+                    #"c_alternate_email" : company[4],
+                    "c_phone" : company[5],
+                    "c_description" : company[6],
+                }
+
+                history[_[0]] = {
+                    "job_details" : job_details,
+                    "status" : s,
+                    "company_details" : company_details,
+                }
+                
+    data["jobs"] = history
     return render(request, "dashboard.html", data)
 
 
@@ -34,7 +89,66 @@ def applicant_profile(request):
     return render(request, "applicant_profile.html", data)
 
 def applicant_history(request):
+    """ Renders Applicant History """
+    global data
+    
+    history = dict()
+    # Fetching history
+    with connection.cursor() as cursor :
+        cursor.execute("SELECT * FROM application_status WHERE applicant_status LIKE '%{}%'".format(request.session["id"]))
+        result = cursor.fetchall()
+        #print(result)
+        for _ in result:
+            # for i in _:
+            #     print(i)
+            cursor.execute("SELECT * FROM jobs WHERE j_id = {}".format(_[0]))
+            status = eval(_[1])
+            #status = eval(status[request.session["id"]])
+            #print(status[request.session["id"]]["status"])
+            s = ""
+            if int(status[request.session["id"]]["status"]) == 0:
+                s = "Applied"
+            elif int(status[request.session["id"]]["status"]) == 1:
+                s = "Accepted"
+            elif int(status[request.session["id"]]["status"]) == 2:
+                s = "Rejected"
+            elif int(status[request.session["id"]]["status"]) == 3:
+                s = "On Hold"
+            else:
+                pass
+            
+            #print(cursor.fetchall())
+            res = cursor.fetchall()
+
+            for i in res:
+                job_details = eval(i[2])
+                #print(job_details)
+            
+                cursor.execute("SELECT * FROM company WHERE c_id = {}".format(i[1]))
+                #print(cursor.fetchall())
+                company = cursor.fetchall()[0]
+                company_details = {
+                    "c_id" : company[0],
+                    "c_name" : company[1],
+                    "c_address" : company[2],
+                    "c_email" : company[3],
+                    #"c_alternate_email" : company[4],
+                    "c_phone" : company[5],
+                    "c_description" : company[6],
+                }
+
+                history[_[0]] = {
+                    "job_details" : job_details,
+                    "status" : s,
+                    "company_details" : company_details,
+                }
+                
+    data["jobs"] = history
     return render(request, "applicant_history.html", data)
+
+# ((8, 20, "{'designation' : 'Department Head','description' : 'descp8', 
+# 'skills' : '('Java','SQL','HTML')','experience' : '5years', 'qualification' : '('HSC')'}", 
+# 9, 8.0, datetime.date(2019, 11, 12)),)
 
 def browse_jobs_render(request):
     return render(request, "browse_jobs.html", data)
